d31c9b67b89367d3c6a0090c3f452f52ea641488
Nikunj Gurjar <39230292+Designicks@users.noreply.github.com>
2019-04-16 11:20:01 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,73 @@
+<footer class="main-footer">
+      <div class="container">
+        <div class="row">
+          <div class="about col-md-4">
+            <div class="logo"><img src="img/logo/logo.png" alt="logo" class="img-fluid"></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          </div>
+          <div class="site-links col-md-4">
+            <h3>Useful links</h3>
+            <div class="menus d-flex">
+              <ul class="list-unstyled">
+                <li> <a href="index.php">Homepage</a></li>
+                <li> <a href="category.html">Listings detail</a></li>
+                <li> <a href="detail.html">Listing detail</a></li>
+                <li> <a href="blog.html">Blog</a></li>
+                <li> <a href="post.html">Post</a></li>
+                <li> <a href="#">Privacy policy </a><span class="badge badge-secondary text-uppercase ml-1">Soon</span></li>
+              </ul>
+              <ul class="list-unstyled">
+                <li> <a href="text.html">Text page</a></li>
+                <li> <a href="404.html">404 - Not found</a></li>
+                <li> <a href="contact.html">Contact</a></li>
+                <li> <a href="#">Pricing </a><span class="badge badge-secondary text-uppercase ml-1">Soon</span></li>
+              </ul>
+            </div>
+          </div>
+          <div class="contact col-md-4">
+            <h3>Contact  us</h3>
+            <div class="info">
+              <p>53 Broadway, Broklyn, NY 11249</p>
+              <p>Phone: (020) 123 456 789</p>
+              <p>Email: <a href="mailto:info@company.com">Info@Company.com</a></p>
+              <ul class="social-menu lisy-inline">
+                <li class="list-inline-item">
+                  <a href="#" target="_blank" title="Facebook">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a href="#" target="_blank" title="Twitter">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                  </li>
+                <li class="list-inline-item">
+                  <a href="#" target="_blank" title="Pinterest">
+                    <i class="fab fa-pinterest"></i>
+                  </a>
+                  </li>
+                <li class="list-inline-item">
+                  <a href="#" target="_blank" title="Instagram">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                  </li>
+                <li class="list-inline-item">
+                  <a href="#" target="_blank" title="Vimeo">
+                    <i class="fab fa-vimeo"></i>
+                  </a>
+                  </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyrights text-center">
+        <p>&copy; 2019 - 2020<span class="text-primary">EDESIGNER Pvt. Ltd..</span> All Rights Reserved.</p>
+      </div>
+    </footer>
+    <!-- JavaScript files-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>EDESIGNER Technologies Pvt. Ltd. </title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Google fonts - Poppins-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <!-- Owl Carousel-->
+    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!--Font awesome latest version-->
+    <link rel="stylesheet" type="text/css" href="fontawesome-free-5.7.2-web\css\all.css">
+    <!-- Custom Font Icons CSS-->
+    <link rel="stylesheet" href="css/custom-fonticons.css">
+  </head>
+  <body>
+    <header class="header">
+      <!-- Top bar-->
+      <div class="top-bar">
+        <div class="container">
+          <div class="content-holder d-flex justify-content-between">
+            <div class="info d-flex">
+              <div class="contact d-flex">
+                <p><a href="mailto:info@company.com"> <i class="fas fa-envelope text-primary"></i><span class="d-none d-md-inline">info@company.com</span></a></p>
+                <p> <i class="fa fa-phone text-primary"></i><span class="d-none d-md-inline">020 123-456-789</span></p>
+              </div>
+            </div>
+            <div class="CTAs"><a href="login.php" class="login"><i class="fas fa-sign-in-alt"></i><span class="d-none d-md-inline">Login</span></a><a href="profile.php" class="login"> <i class="fa fa-user"></i><span class="d-none d-md-inline">Profile</span></a>
+            <a href="edit_profile.php" class="login"> <i class="fas fa-pencil-alt"></i><span class="d-none d-md-inline">Edit Profile</span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Main Navbar-->
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <!-- Navbar Brand --><a href="index.php" class="navbar-brand"><img src="img/logo/logo.png" alt="logo" class="img-fluid"></a>
+          <!-- Toggle Button-->
+          <button type="button" data-toggle="collapse" data-target="#navbarcollapse" aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span>Menu</span><i class="fa fa-bars"></i></button>
+          <!-- Navbar Menu -->
+          <div id="navbarcollapse" class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+              <!-- Search-->
+              <li class="nav-item"><a href="index.php" class="nav-link active">Home</a>
+              </li>
+              <li class="nav-item"><a href="read_complete.php" class="nav-link">Product</a>
+              </li>
+              <li class="nav-item"><a href="write_blog.php" class="nav-link">Blog</a>
+              </li>
+              <li class="nav-item"><a href="payment.php" class="nav-link">Payment</a>
+              </li>
+            </ul><a href="add_review.php" class="btn navbar-btn btn-outline-primary mt-3 mt-lg-0 ml-lg-3">Write a Review</a>
+          </div>
+        </div>
+      </nav>
+    </header>
